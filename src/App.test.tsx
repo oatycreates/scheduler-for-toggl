c1f163e17dc199b0966ba6a28fa7de2bdd3e7d76
Patrick Ferguson <patrick.ferguson@scann3d.com.au>
2017-05-10 23:48:28 +1000

    Resolve dependency version issue with jest-enzyme and set up a basic test for App.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App")).toHaveLength(1);
+  });
 });
+
