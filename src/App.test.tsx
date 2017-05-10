174e0236c11a949cfe687caf96edc22dd61573a9
Patrick Ferguson <patrick@ferguson.cloud>
2017-05-11 00:31:17 +1000

    Update code files to match new tslin.json rules.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,10 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import App from './App';
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
 
 describe('App', () => {
   it('renders without crashing', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(".App")).toHaveLength(1);
-  });
-});
-
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('.App')).toHaveLength(1)
+  })
+})
--- src/index.test.tsx
+++ src/index.test.tsx
@@ -2,11 +2,10 @@ describe('Index', () => {
   beforeEach(() => {
     // Mock a DOM mount point
     document.body.innerHTML =
-      '<div id="root"></div>';
-  });
+      '<div id="root"></div>'
+  })
 
   it('renders without crashing', () => {
-    require('./index');
-  });
-});
-
+    require('./index')
+  })
+})
