9f4f49c4ce54ed79241aef007a7e0ffef327280b
Patrick Ferguson <patrick.ferguson@scann3d.com.au>
2017-05-20 14:49:06 +1000

    Add font-family specifiers, Roboto for headings and Open Sans for body text.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
+
+// Site styles
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
 ReactDOM.render(
