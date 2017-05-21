fc645c46bdc194a99cb1d5e76c363610479d4b4c
Patrick Ferguson <patrick@ferguson.cloud>
2017-05-21 16:13:48 +1000

    Add Bootstrap style customisation to Button component.

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
+import * as _ from 'lodash'
 import { shallow } from 'enzyme'
-import Button from './Button'
+import { Button, Styles, getClassNameForStyle } from './Button'
 
 describe('Button', () => {
   it('renders without crashing', () => {
@@ -15,5 +16,19 @@ describe('Button', () => {
     const wrapper = shallow(<Button onClick={testOnClick} />)
     wrapper.find('.Button').simulate('click')
     expect(buttonClickCount).toEqual(1)
+  })
+  describe('with a customised style', () => {
+    // Generate a test for each style type
+    _.each(Styles, (style: Styles) => {
+      // The Styles enum object will have two sets of keys, one for the actual
+      // string values e.g. 'default', 'primary', etc. and one for numeric indexes.
+      // We're only interested in iterating over the set once, pick the strings
+      if (typeof(style) === 'string' && Styles.hasOwnProperty(style)) {
+        it(`Style.${style} causes \'${getClassNameForStyle(style)}\' to appear on button`, () => {
+          const wrapper = shallow(<Button style={style} />)
+          expect(wrapper.find('.Button')).toHaveClassName(getClassNameForStyle(style))
+        })
+      }
+    })
   })
 })
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,16 +2,58 @@ import * as React from 'react'
 
 import './Button.css'
 
+export enum Styles {
+  default,
+  primary,
+  success,
+  info,
+  warning,
+  danger,
+  link,
+}
+
 export interface ButtonProps {
   buttonText?: string,
-  disabled?: boolean
-  onClick?: React.EventHandler<React.MouseEvent<HTMLButtonElement>>
+  disabled?: boolean,
+  onClick?: React.EventHandler<React.MouseEvent<HTMLButtonElement>>,
+  style?: Styles,
+}
+
+/**
+ * Returns the Bootstrap class name for the input button styling type.
+ * @param style Styling to use for the button.
+ */
+export const getClassNameForStyle = (style: Styles | undefined) => {
+  switch (style) {
+    case Styles.default:
+      return 'btn-default'
+    case Styles.primary:
+      return 'btn-primary'
+    case Styles.success:
+      return 'btn-success'
+    case Styles.info:
+      return 'btn-info'
+    case Styles.warning:
+      return 'btn-warning'
+    case Styles.danger:
+      return 'btn-danger'
+    case Styles.link:
+      return 'btn-link'
+    default:
+      return 'btn-default'
+  }
 }
 
 export const Button: React.StatelessComponent<ButtonProps> = (props) => {
+  const cssClasses = [
+    'Button',
+    'btn',
+    getClassNameForStyle(props.style),
+  ].join(' ')
+
   return (
     <button
-      className="Button"
+      className={cssClasses}
       type="button"
       disabled={props.disabled}
       onClick={props.onClick}
@@ -23,6 +65,7 @@ export const Button: React.StatelessComponent<ButtonProps> = (props) => {
 Button.defaultProps = {
   buttonText: 'Submit',
   disabled: false,
+  style: Styles.default,
 }
 
 export default Button
