4e2d54f0ff93998a9a90d215a8f4242eda56d279
Patrick Ferguson <patrick@ferguson.cloud>
2017-05-21 22:57:57 +1000

    Fix enum indexing in Button.test.tsx generated styles tests.

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,3 +1,4 @@
+import 'jest-enzyme'
 import * as React from 'react'
 import * as _ from 'lodash'
 import { shallow } from 'enzyme'
@@ -17,16 +18,21 @@ describe('Button', () => {
     wrapper.find('.Button').simulate('click')
     expect(buttonClickCount).toEqual(1)
   })
+
   describe('with a customised style', () => {
     // Generate a test for each button style type
     _.each(ButtonStyles, (buttonStyle: ButtonStyles) => {
       // The ButtonStyles enum object will have two sets of keys, one for the actual
       // string values e.g. 'default', 'primary', etc. and one for numeric indexes.
-      // We're only interested in iterating over the set once, pick the strings
-      if (typeof(buttonStyle) === 'string' && ButtonStyles.hasOwnProperty(buttonStyle)) {
-        it(`Style.${buttonStyle} causes \'${getClassNameForStyle(buttonStyle)}\' to appear on button`, () => {
+      // We're only interested in iterating over the set once, pick the indexes
+      // as that's what the enum appears to be compiled to under the hood
+      if (typeof(buttonStyle) === 'number') {
+        const className = getClassNameForStyle(buttonStyle)
+        // Because we're actually using an index here, the index is needed to
+        // access the key name in ButtonStyles
+        it(`Style.${ButtonStyles[buttonStyle]} causes \'${className}\' to appear on button`, () => {
           const wrapper = shallow(<Button buttonStyle={buttonStyle} />)
-          expect(wrapper.find('.Button')).toHaveClassName(getClassNameForStyle(buttonStyle))
+          expect(wrapper.find('.Button')).toHaveClassName(className)
         })
       }
     })
