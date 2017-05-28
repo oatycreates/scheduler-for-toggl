1c0ba813f24852323160e3a5036ccaa2531a41dd
Patrick Ferguson <patrick@ferguson.cloud>
2017-05-28 20:02:50 +1000

    Add basic test for ApiTokenField container component.

---
+++ src/containers/ApiTokenField/container.test.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import * as _ from 'lodash'
+import { render } from 'enzyme'
+import { Store, Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { SchedulerForTogglAppState } from '../../reducers/'
+import ApiTokenField from './container'
+
+// Initialise a mocked Redux store with relevant middleware
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+describe('ApiTokenField container', () => {
+  let store: Store<SchedulerForTogglAppState>
+  let container
+  const initialState: Partial<SchedulerForTogglAppState> = {
+    apiToken: {
+      apiToken: '',
+      error: '',
+      isValid: false,
+      isValidating: false,
+    },
+  }
+
+  beforeEach(() => {
+    store = mockStore(initialState)
+    container = render(<Provider store={store}><ApiTokenField /></Provider> )
+  })
+
+  it('renders without crashing', () => {
+    expect(container.find('.ApiTokenField')).toHaveLength(1)
+  })
+})
