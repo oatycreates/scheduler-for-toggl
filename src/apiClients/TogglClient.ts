df038f7af75ed03b0b0c85bb31656a7aa93f2709
Patrick Ferguson <patrick@ferguson.cloud>
2017-06-02 01:14:12 +1000

    Remove unnecessary extra initialisation check variable from TogglClient single instance script.

--- src/apiClients/TogglClient.ts
+++ src/apiClients/TogglClient.ts
@@ -1,20 +1,21 @@
 import * as TogglClient from 'toggl-api'
 
-let togglClientInitialised: boolean = false
-let togglClientInstance: TogglClient
+let togglClientInstance: TogglClient | null = null
 /**
  * Initialises the ToggleClient singleton instance with the provided options.
  * @param options Options to configure the Toggl API Client.
  *   See: https://7eggs.github.io/node-toggl-api/TogglClient.html
  */
 export const initTogglClient = (options: TogglClient.TogglClientOptions) => {
-  togglClientInstance = new TogglClient(options)
-  togglClientInitialised = true
+  if (!togglClientInstance) {
+    togglClientInstance = new TogglClient(options)
+  }
+  return togglClientInstance
 }
 
 // All scripts that import and utilise this getter will be using the same instance
 export const getTogglClient = () => {
-  if (!togglClientInitialised) {
+  if (!togglClientInstance) {
     throw Error('TogglClient hasn\'t been initialised before getTogglClient() call!')
   }
 
