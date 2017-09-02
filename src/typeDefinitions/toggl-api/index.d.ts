bc28ed2576d7531fe264a8fc12bac0c84fb64ab7
Patrick Ferguson <patrick@ferguson.cloud>
2017-09-02 14:07:49 +1000

    Improve comment formatting of existing Toggl API data types.

--- src/typeDefinitions/toggl-api/index.d.ts
+++ src/typeDefinitions/toggl-api/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for node-toggl-api v1.0.1
+// Typescript type definitions for Toggl node-toggl-api v1.0.1
 // Project: https://github.com/7eggs/node-toggl-api
 // Definitions by: Patrick Ferguson <https://github.com/patferguson>
 
@@ -28,22 +28,46 @@ declare module 'toggl-api' {
     /**
      * Properties within the 'data' field are used.
      * See: https://github.com/toggl/toggl_api_docs/blob/master/chapters/users.md#get-current-user-data
-    */
+     */
     export interface UserDataResponse {
+      /**
+       * User ID
+       */
       "id": number,
+      /**
+       * User's API token
+       */
       "api_token": string,
+      /**
+       * Default workspace ID
+       */
       "default_wid": number,
+      /**
+       * User's email
+       */
       "email": string,
+      /**
+       * User's name
+       */
       "fullname": string,
       "jquery_timeofday_format": string,
       "jquery_date_format": string,
       "timeofday_format": string,
+      /**
+       * Date format string (for display?)
+       */
       "date_format": string,
       "store_start_and_stop_time": boolean,
+      /**
+       * Day index considered the start of the week?
+       */
       "beginning_of_week": number,
       "language": string,
       "image_url": string,
       "sidebar_piechart": boolean,
+      /**
+       * For responses, indicates the time the object was last updated, ISO 8601 date and time format.
+       */
       "at": string,
       "retention": number,
       "record_timeline": boolean,
@@ -58,17 +82,50 @@ declare module 'toggl-api' {
      * See: https://github.com/toggl/toggl_api_docs/blob/master/chapters/time_entries.md
      */
     export interface TimeEntry {
-      "description": string, // (string, strongly suggested to be used)
-      "wid"?: number, // workspace ID (integer, required if pid or tid not supplied)
-      "pid"?: number, // project ID (integer, not required)
-      "tid"?: number, // task ID (integer, not required)
-      "billable"?: boolean, // (boolean, not required, default false, available for pro workspaces)
-      "start": string, // time entry start time (string, required, ISO 8601 date and time)
-      "stop"?: string, // time entry stop time (string, not required, ISO 8601 date and time)
-      "duration": number, // time entry duration in seconds. If the time entry is currently running, the duration attribute contains a negative value, denoting the start of the time entry in seconds since epoch (Jan 1 1970). The correct duration can be calculated as current_time + duration, where current_time is the current time in seconds since epoch. (integer, required)
-      "tags"?: Array<string>, // a list of tag names (array of strings, not required)
-      "duronly"?: boolean, // should Toggl show the start and stop time of this time entry? (boolean, not required)
-      "at"?: string, // timestamp that is sent in the response, indicates the time item was last updated
+      /**
+       * Entry description (string, strongly suggested to be used)
+       */
+      "description": string,
+      /**
+       * Workspace ID (integer, required if pid or tid not supplied)
+       */
+      "wid"?: number,
+      /**
+       * Project ID (integer)
+       */
+      "pid"?: number,
+      /**
+       * Task ID (integer)
+       */
+      "tid"?: number,
+      /**
+       * Whether the entry is billable to the client (boolean, default false, available for pro workspaces)
+       */
+      "billable"?: boolean,
+      /**
+       * Time entry start time (string, required, ISO 8601 date and time format)
+       */
+      "start": string,
+      /**
+       * Time entry stop time (string, ISO 8601 date and time format)
+       */
+      "stop"?: string,
+      /**
+       * Time entry duration in seconds. If the time entry is currently running, the duration attribute contains a negative value, denoting the start of the time entry in seconds since epoch (Jan 1 1970). The correct duration can be calculated as current_time + duration, where current_time is the current time in seconds since epoch. (integer, required)
+       */
+      "duration": number,
+      /**
+       * A list of tag names (array of strings)
+       */
+      "tags"?: Array<string>,
+      /**
+       * Should Toggl show the start and stop time of this time entry? (boolean)
+       */
+      "duronly"?: boolean,
+      /**
+       * For responses, indicates the time the object was last updated, ISO 8601 date and time format.
+       */
+      "at"?: string,
     }
 
     /**
